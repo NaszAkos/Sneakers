02a43cc56245643467107184fd255a66f8afbe08
adolfaron <150439314+adolfaron@users.noreply.github.com>
2025-02-14 19:39:45 +0100

    fejléc animáció

--- js/app.js
+++ js/app.js
@@ -8,11 +8,49 @@ function fejlec_meretez() {
             const header = iframe.contentDocument.getElementById("mind");
             if (header) {
                 iframe.style.height = header.offsetHeight + "px";
-                document.getElementById("fejlec_tak").style.height = header.offsetHeight + "px";
+                //document.getElementById("fejlec_tak").style.display = "block"
+                //document.getElementById("fejlec_tak").style.height = header.offsetHeight + "px";
             }
         }, 100);
     }
 }
+let lastScrollTop = 0; // Keep track of last scroll position
+const navbar = document.getElementById("fejlec");
+
+window.addEventListener("scroll", function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        navbar.style.top = "-60px";
+        //navbar.style.borderBottom = "1px solid black"
+        if (currentScroll > 60) {
+            //navbar.style.borderBottom = "1px solid black"
+            //navbar.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)"
+        } else {
+            navbar.style.borderBottom = "none"
+            navbar.style.boxShadow = "none"
+        }
+        
+    } else {
+        navbar.style.top = "0";
+        //navbar.style.borderBottom = "none"
+        if (currentScroll > 0) {
+            //navbar.style.borderBottom = "1px solid black"
+            navbar.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)"
+            
+        } else {
+            navbar.style.borderBottom = "none"
+            navbar.style.boxShadow = "none"
+        }
+    }
+    setTimeout(function() {
+        if (navbar.style.top === "-60px") {
+            navbar.style.boxShadow = "none"
+        }
+    }, 300);
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
+
 function lablec_meretez(){
     iframe = document.getElementById("lablec");
 
@@ -29,6 +67,26 @@ window.addEventListener('resize', function() {
     fejlec_meretez()
     lablec_meretez()
   });
+
+/*document.addEventListener("DOMContentLoaded", function () {
+    let currentPage = window.location.pathname; // Az aktuális oldal útvonala
+    let elem = document.getElementById("fejlec");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) { 
+            if (currentPage === "/" && !currentPage.includes("fooldal.html")) {
+                elem.style.borderBottom = "1px solid black"; 
+            } else {
+                elem.style.borderBottom = "none";
+            }
+        } else {
+            elem.style.borderBottom = "none"; 
+        }
+    });
+
+    
+});
+
 
   /*const eredetiUrl = window.location.pathname; // Eredeti URL mentése 
   document.addEventListener("DOMContentLoaded", function () {
---
+++ js/cipo.js
@@ -0,0 +1,62 @@
+szin_ell(nev)
+      function kosar(nev){
+        if (localStorage.getItem(nev) === "1") {
+          localStorage.setItem(nev, '0');
+          szin_ell(nev)
+        } else {
+          localStorage.setItem(nev, '1');
+          window.top.location.href = "/kosár.html";
+          szin_ell(nev)
+        }
+      }
+      function szin_ell(nev) {
+      if (localStorage.getItem(nev) === "1") {
+          document.getElementById("gomb").innerText = "Törlés a kosárból"
+          document.getElementById("gomb").style.backgroundColor = "#c62d2d"
+          document.getElementById("gomb").style.height="120px"
+      } else {
+        document.getElementById("gomb").innerText = "Kosárba"
+        document.getElementById("gomb").style.backgroundColor = "#53E558"
+        document.getElementById("gomb").style.height="70px"
+      }}
+
+    function kepkatt(){
+      // Az összes olyan elem, aminek class attribútuma "szoveg" vagy "also"
+      const elements = document.querySelectorAll('[class*="szoveg"], [class*="also"], [id*="lablec"], [id*="fejlec"], [class*="logo"], [ class*="vonal-vizsz"]');
+        elements.forEach(element => {
+        element.style.display = 'none'; // Eltüntetjük
+        const carousel = document.getElementById('carouselExampleDark'); 
+        //carousel.style.backgroundColor="rgba(183, 60, 60, 0.5)";
+        carousel.style.width = '100vw';
+        carousel.style.height = '100vh';
+        carousel.style.top = '0';
+        carousel.style.left = '0';
+        carousel.style.zIndex = '3';    
+    });
+    //document.querySelectorAll('class*=kepek').style.height = '100vh'
+    closeButton.style.display = 'block';
+    //document.querySelectorAll('.kep img').style.width = '40px' 
+  }
+  function kep_bezar() {
+    const carousel = document.getElementById('carouselExampleDark');
+    const closeButton = document.getElementById('closeButton');
+    
+    // Visszaállítjuk az alapértelmezett állapotot
+    const elements = document.querySelectorAll('[class*="szoveg"], [class*="also"], [id*="lablec"], [id*="fejlec"], [class*="logo"], [ class*="vonal-vizsz"]');
+        elements.forEach(element => {
+    element.style.display = '';
+    carousel.style.width = '';
+    carousel.style.height = '';
+    carousel.style.position = '';
+    carousel.style.top = '';
+    carousel.style.left = '';
+    carousel.style.zIndex = '';
+    
+    
+    
+        });
+        // Elrejtjük a zárás gombot
+    
+    closeButton.style.display = 'none';
+    document.getElementById('fejlec_tak').style.display = 'none'
+  }
