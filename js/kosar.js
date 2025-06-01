cf1a04e65737a208fdcb448117ccb37f548e0ef8
unknown <adolfaron@turr.hu>
2025-06-01 16:53:45 +0200

    kosár dinamikos ajálnlás

--- js/kosar.js
+++ js/kosar.js
@@ -8,6 +8,14 @@ sessionStorage.setItem("YB", "1")
 sessionStorage.setItem("YB", "1")
 sessionStorage.setItem("YB", "1")*/
 
+window.onload = () => {
+  ajanlasok_bet();
+  script = document.createElement("script");
+  script.src = "../js/app.js";
+  document.body.appendChild(script);
+};
+
+termekek = []
 let kosar_ft = 0;
 tartalom_ell()
 async function tartalom_ell() {
@@ -55,6 +63,26 @@ document.getElementById('fejlec').onload=function(){
   const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
   const element = iframeDocument.getElementById('kosrikon');
   element.style.display = 'none';}
+
+window.addEventListener("message", function(event) {
+  if (event.data === "modvaltas") {
+    sleep(100)
+      const iframe = document.getElementById('fejlec');
+      const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+      const element = iframeDocument.getElementById('kosarikon');
+      element.style.display = 'none';
+  }
+});
+
+setInterval(() => {
+  const iframe = document.getElementById('fejlec');
+  if (!iframe.contentDocument) return;
+  const elem = iframe.contentDocument.getElementById('kosrikon');
+  if (elem) elem.style.display = 'none';
+}, 300);
+
+
+
 kupon_bev()
 function kupon_bev() {
   let productPrice = kosar_ft;
@@ -81,3 +109,72 @@ function kupon_bev() {
 }
 // Meghívjuk a tartalom_ell függvényt, ha van változás a sessionStorage-ban
 //window.onload = tartalom_ell; // Az oldal betöltésekor is frissíti a megjelenítést
+
+async function ajanlasok_bet() {
+    const response2 = await fetch('data/cipok.json');
+    const termekek2 = await response2.json();
+    console.log("termekek2 tartalma:", termekek2);
+    const ajanlas = document.getElementById("ajanlas");
+    const megjelen = ["NAF1", "AS", "YB", "EA7t"];
+
+    termekek2.forEach(termek => {
+        const db = termek.rid;
+        console.log("RID:", db); // <- ez eddig nem futott le
+
+        if (db && megjelen.includes(db)) {
+            console.log("Megjelenítendő:", db);
+
+            const a = document.createElement("a");
+            a.className = "col-xs-11 col-sm-11 col-md-11 col-lg-5 col-xl-5 col-xxl-5";
+            a.href = `cipok/cipok.html?id=${termek.id}`;
+            a.innerHTML = `
+                <img class="Cipo" src="${termek.kepek[0].slice(3)}" alt="${termek.nev}">
+                <h3>${termek.nev}<br>
+                    ${new Intl.NumberFormat('fr-FR').format(termek.ar)} Ft</h3>
+            `;
+
+            ajanlas.appendChild(a);
+
+            const hr = document.createElement("hr");
+            hr.className = "tel_valaszto";
+            ajanlas.appendChild(hr);
+        }
+    });
+  
+}
+
+
+  /* function ajanlasok_bet() {
+    console.log("indul")
+    fetch('data/cipok.json')
+        .then(response => response.json())
+        .then(termekek => {
+            const kosarLista = document.getElementById("ajanlas");
+            const megjelen = ["NAF1", "AS", "YB", "EA7t"];
+
+            termekek.forEach(termek => {
+                const db = termek.rid;
+                console.log("RID:", db);
+
+                if (db && megjelen.includes(db)) {
+                    console.log("Megjelenítendő:", db);
+
+                    const a = document.createElement("a");
+                    a.className = "col-xs-11 col-sm-11 col-md-11 col-lg-5 col-xl-5 col-xxl-5";
+                    a.href = `cipok/cipok.html?id=${termek.id}`;
+                    a.innerHTML = `
+                        <img class="Cipo" src="${termek.kepek[0].slice(3)}" alt="${termek.nev}">
+                        <h3>${termek.nev}<br>
+                            ${new Intl.NumberFormat('fr-FR').format(termek.ar)} Ft</h3>
+                    `;
+
+                    kosarLista.appendChild(a);
+
+                    const hr = document.createElement("hr");
+                    hr.className = "tel_valaszto";
+                    kosarLista.appendChild(hr);
+                }
+            });
+        })
+        .catch(error => console.error("Hiba történt a JSON betöltés közben:", error));
+} */
